e43970978b34d35352a10320ccb92355268faa7d
quangTD <tsquang398@gmail.com>
2021-09-29 05:40:20 +0700

    vid: refactor code - checkNumber in randomPerson fn

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -26,12 +26,7 @@ const Review = () => {
     if (randomNum === index) {
       randomNum = index + 1;
     }
-    console.log(
-      "🚀TCL: ~ file: Review.js ~ line 26 ~ randomPerson ~ randomNum",
-      randomNum
-    );
-
-    setIndex(randomNum);
+    setIndex(checkNumber(randomNum));
   };
   return (
     <section className="container">
@@ -42,6 +37,9 @@ const Review = () => {
       <article className="review">
         <div className="img-container">
           <img className="person-img" src={image} alt={name} />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
         </div>
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>

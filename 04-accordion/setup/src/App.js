b77c87cb4dc9d0da25230e6a6f73ce62519c3e1c
quangTD <tsquang398@gmail.com>
2021-09-29 06:22:00 +0700

    myseft: handle toggle arcoodion

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import Question from "./Question";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [faqData, setFaqData] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section>
+          {faqData &&
+            faqData.map(({ id, title, info }) => (
+              <Question key={id} title={title} info={info} />
+            ))}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [isVisible, setVisible] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setVisible(!isVisible)}>
+          {isVisible ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      <p className={`info ${isVisible ? "active" : ""}`}>{info}</p>
+    </article>
+  );
 };
 
 export default Question;

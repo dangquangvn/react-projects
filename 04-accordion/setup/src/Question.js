b0b48806ba06dbd90c1a0e833810bd6418e3c367
quangTD <tsquang398@gmail.com>
2021-09-29 13:39:20 +0700

    myseft: enhance animate when show/hide accordion

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -3,14 +3,14 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
   const [isVisible, setVisible] = useState(false);
   return (
-    <article className="question">
+    <article className={`question ${isVisible ? "active" : ""}`}>
       <header>
         <h4>{title}</h4>
         <button className="btn" onClick={() => setVisible(!isVisible)}>
           {isVisible ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {isVisible && <p>{info}</p>}
+      <p className="info2">{info}</p>
     </article>
   );
 };

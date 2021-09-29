d20574705da9a2493817d993b6c650c88c693140
quangTD <tsquang398@gmail.com>
2021-09-29 17:39:48 +0700

    implement UI

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  return (
+    <main>
+      <Categories items={items} />
+      <Menu items={items} />
+    </main>
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ items }) => {
+  const uniqueItems = [...new Set(items.map((item) => item.category))];
+  console.log(
+    "🚀TCL: ~ file: Categories.js ~ line 5 ~ Categories ~ uniqueItems",
+    uniqueItems
+  );
+  return (
+    <section className="menu section">
+      <div className="title">
+        <h2>menu project</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="btn-container">
+        <button type="button" className="filter-btn">
+          All
+        </button>
+        {uniqueItems &&
+          uniqueItems.map((category) => (
+            <button type="button" className="filter-btn" key={category}>
+              {category}
+            </button>
+          ))}
+      </div>
+    </section>
+  );
 };
 
 export default Categories;

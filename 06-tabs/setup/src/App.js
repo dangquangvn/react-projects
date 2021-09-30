0918dac4409ffc5533047d1ef8a34cf5e2a758d5
quangTD <tsquang398@gmail.com>
2021-09-30 15:57:56 +0700

    vid: refactor code

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -5,23 +5,17 @@ import Loading from "./Loading";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tabs-project";
 function App() {
-  const [index, setIndex] = useState(0);
-  console.log("🚀TCL: ~ file: App.js ~ line 9 ~ App ~ index", index);
-  const [jobData, setJobData] = useState([]);
-  console.log(
-    "🚀TCL: ~ file: App.js ~ line 9 ~ App ~ jobData",
-    jobData,
-    jobData[0]
-  );
   const [isLoading, setLoading] = useState(true);
+  const [jobData, setJobData] = useState([]);
+  const [index, setIndex] = useState(0);
   // fetch api
   const fetchJob = async () => {
     setLoading(true);
     try {
       const res = await fetch(url);
       const data = await res.json();
-      setLoading(false);
       setJobData(data);
+      setLoading(false);
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -30,45 +24,41 @@ function App() {
   useEffect(() => {
     fetchJob();
   }, []);
+  if (isLoading) {
+    return <Loading />;
+  }
+  const { company, dates, duties, title } = jobData[index];
   return (
-    <section>
+    <section className="section">
       <div className="title">
         <h2>Tab Project - CV</h2>
         <div className="underline"></div>
       </div>
-      {!jobData || isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <div className="jobs-center">
-            <div className="btn-containers">
-              {jobData.map(({ id, company }, i) => (
-                <button
-                  className={`job-btn ${index === i ? "active-btn" : ""}`}
-                  key={id}
-                  onClick={() => {
-                    setIndex(i);
-                  }}
-                >
-                  {company}
-                </button>
-              ))}
+      <div className="jobs-center">
+        <div className="btn-containers">
+          {jobData.map((item, i) => (
+            <button
+              className={`job-btn ${index === i ? "active-btn" : ""}`}
+              key={item.id}
+              onClick={() => setIndex(i)}
+            >
+              {item.company}
+            </button>
+          ))}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, i) => (
+            <div className="job-desc" key={i}>
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{duty}</p>
             </div>
-            <article className="job-info">
-              <h3>{jobData[index]?.title}</h3>
-              <h4>{jobData[index]?.company}</h4>
-              <p className="job-date">{jobData[index]?.dates}</p>
-              {jobData[index]?.duties.map((duty, i) => (
-                <div className="job-desc" key={i}>
-                  <FaAngleDoubleRight className="job-icon" />
-                  <p>{duty}</p>
-                </div>
-              ))}
-            </article>
-          </div>
-          <button className="btn">more info</button>
-        </>
-      )}
+          ))}
+        </article>
+      </div>
+      <button className="btn">more info</button>
     </section>
   );
 }

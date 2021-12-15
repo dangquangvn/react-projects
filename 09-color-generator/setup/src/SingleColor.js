2f8ec70e0c14039436f000d3f63bca2edb3f7b05
quangTD <tsquang398@gmail.com>
2021-12-15 13:23:36 +0700

    :coffin: clean unused copyToClipboard ver1

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -10,36 +10,9 @@ const SingleColor = ({ colorShade }) => {
   // const [isCopied, setIsCopied] = useState(false);
   const [isCopied, handleCopy] = useCopyToClipboard(5000);
 
-  //* copy text to clipboard func
-  // async function copyTextToClipboard(text) {
-  //   if ("clipboard" in navigator) {
-  //     return await navigator.clipboard.writeText(text);
-  //   } else {
-  //     return document.execCommand("copy", true, text);
-  //   }
-  // }
-
-  //handle copy click
-  // const handleCopyClick = async () => {
-  //   console.log(
-  //     "🚀TCL: ~ file: SingleColor.js ~ line 51 ~ SingleColor ~ timeout",
-  //     timeout
-  //   );
-  //   try {
-  //     const text = await copyTextToClipboard(`#${hex}`);
-  //     setIsCopied(true);
-  //     // clearTimeout(timeout);
-  //     // timeout = setTimeout(() => {
-  //     //   setIsCopied(false);
-  //     // }, 5000);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
   const handleCopyClick = () => {
     return handleCopy(`#${hex}`);
   };
-  // useDebounce(isCopied, 5000, () => setIsCopied(false));
 
   return (
     <article

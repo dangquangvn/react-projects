36fc6c1a6ff95f6c69cfc96db0da19334082ecac
quangTD <tsquang398@gmail.com>
2021-12-15 13:18:02 +0700

    🌟feat: store color in localstorage

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -5,30 +5,38 @@ import Values from "values.js";
 import { useEffect } from "react";
 
 function App() {
-  const [color, setColor] = useState("#f15025");
+  const [color, setColor] = useState(
+    `${localStorage.getItem("color") || "#f15025"}`
+  );
   const [error, setError] = useState(false);
   // list
-  const [colorArray, setColorArray] = useState([]);
+  const [colorArray, setColorArray] = useState(
+    new Values(`${localStorage.getItem("color") || "#f15025"}`).all(10)
+  );
+  // const [colorArray, setColorArray] = useState("");
+  console.log(localStorage.getItem("color"));
 
   // render first time get in
-  useEffect(() => {
-    if (!color) return;
-    const renderColor = new Values(color);
-    setColorArray(renderColor.all(10));
-    console.log(
-      "🚀TCL: ~ file: App.js ~ line 9 ~ App ~ colorArray",
-      colorArray
-    );
-  }, []);
+  // useEffect(() => {
+  //   if (!color) return;
+  //   const renderColor = new Values(color);
+  //   setColorArray(renderColor.all(10));
+  //   console.log(
+  //     "🚀TCL: ~ file: App.js ~ line 9 ~ App ~ colorArray",
+  //     colorArray
+  //   );
+  // }, []);
 
   // click submit btn
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(color);
     try {
+      setError(false);
       if (!color) return;
-      const renderColor = new Values(color);
-      setColorArray(renderColor.all(10));
+      localStorage.setItem("color", color);
+      const renderColor = new Values(color).all(10);
+      setColorArray(renderColor);
       console.log(
         "🚀TCL: ~ file: App.js ~ line 9 ~ App ~ colorArray",
         colorArray
@@ -48,8 +56,8 @@ function App() {
             name='color'
             id='color'
             className={`${error ? "error" : null}`}
-            placeholder='#f15025'
-            value={color}
+            placeholder={"#f15025"}
+            value={color || localStorage.getItem("color")}
             onChange={(e) => setColor(e.target.value)}
           />
           <button type='submit' className='btn'>

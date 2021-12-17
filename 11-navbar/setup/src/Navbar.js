17ecb5ccf0dfc131990c3b528ff36f57b80a7fcd
quangTD <tsquang398@gmail.com>
2021-12-17 15:35:57 +0700

    🎉 navbar with useRef change height

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,53 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  // const [showlinks, setShowLinks]
+  const [isExpand, setExpand] = useState(false);
+  const linksRef = useRef(null);
+  const handleToggler = () => {
+    // console.log(linksRef);
+    setExpand(!isExpand);
+  };
+  useEffect(() => {
+    if (isExpand) {
+      linksRef.current.style.height = "200px";
+    } else {
+      linksRef.current.style.height = `0px`;
+    }
+  }, [isExpand]);
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='Coding Addict' className='logo' />
+          <button className='nav-toggle' onClick={handleToggler}>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container show-container' ref={linksRef}>
+          <ul className='links'>
+            {links &&
+              links.map(({ id, url, text }) => (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              ))}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social &&
+            social.map(({ id, url, icon }) => (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;

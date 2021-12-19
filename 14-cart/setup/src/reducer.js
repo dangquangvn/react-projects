7b091b72f57f785503d1b886bda549dc15c83a04
quangTD <tsquang398@gmail.com>
2021-12-19 16:16:44 +0700

    🌟feat: setup reducer and clear cart

---
+++ 14-cart/setup/src/reducer.js
@@ -0,0 +1,11 @@
+import React from "react";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CLEAR_CART":
+      return { ...state, cart: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;

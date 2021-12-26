ac85b4f3893d7c001b5ef231974174a84fcf5771
quangTD <tsquang398@gmail.com>
2021-12-26 12:40:34 +0700

    🌟feat: render cocktail list

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React, { useEffect, useState } from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
-  return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+  const { loading, drinks } = useGlobalContext();
+  const [list, setList] = useState([]);
+  // const drinksList = cocktailList && cocktailList.drinks;
+  // const { drinks } = cocktailList || {};
 
-export default CocktailList
+  useEffect(() => {
+    setList(drinks);
+  }, [drinks]);
+
+  console.log(
+    "🚀TCL: ~ file: CocktailList.js ~ line 11 ~ CocktailList ~ drinks",
+    list
+  );
+  // if (drinks && drinks[0]) {
+  //   console.log(
+  //     "🚀TCL: ~ file: CocktailList.js ~ line 8 ~ CocktailList ~ CocktailList",
+  //     drinks[0]
+  //   );
+  // }
+  return (
+    <section className='section'>
+      <h2 className='section-title'>Cocktails</h2>
+      <div className='cocktails-center'>
+        {list &&
+          list.map((drink) => <Cocktail key={drink.idDrink} {...drink} />)}
+      </div>
+    </section>
+  );
+};
+
+export default CocktailList;

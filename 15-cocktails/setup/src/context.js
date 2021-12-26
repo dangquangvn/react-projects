fa9f6fec1f90eb595b761d1d13eeb14a00aeb76f
quangTD <tsquang398@gmail.com>
2021-12-26 16:57:53 +0700

    :art: improve fetchHook and destructure data

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -7,31 +7,46 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  //searchTerm / searchQuery
   const [textSearch, setTextSearch] = useState("");
-  // const [searchQuery, setSearchQuery] = useState("");
-  // get cocktail data
-  // const { loading, data: cocktailList } = useFetch(`${url}${searchQuery}`);
-  // const { loading, data: cocktailList } = useFetch(`${url}${textSearch}`);
+  const [cocktails, setCocktails] = useState([]);
+
+  //& prevent space or some special character
+  const prepareSearchQuery = (query) => {
+    const urlWithSearch = `${url}${query}`;
+    console.log("prepareSearchQuery");
+    return encodeURI(urlWithSearch);
+  };
+
+  //& get data using useFetch hook
   const {
     loading,
     data: { drinks },
-  } = useFetch(`${url}${textSearch}`);
+  } = useFetch(prepareSearchQuery(textSearch));
 
-  const prepareSearchQuery = (query) => {
-    const urlWithSearch = `${url}${query}`;
-
-    return encodeURI(urlWithSearch);
+  // put data just fetched into state to avoid undefined
+  const getCocktails = () => {
+    if (drinks) {
+      const newCocktails = drinks.map((item) => {
+        const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+          item;
+        return {
+          id: idDrink,
+          name: strDrink,
+          image: strDrinkThumb,
+          info: strAlcoholic,
+          glass: strGlass,
+        };
+      });
+      setCocktails(newCocktails);
+    } else {
+      setCocktails([]);
+    }
   };
-  // get list based on text search
-  // const fetchDataBasedOnSearch = async (text) => {
-  //   const response = await fetch(`${url}${text}`);
-  //   const data = await response.json();
-  //   setData(data);
-  //   setLoading(false);
-  // };
-  // useEffect(() => {
-  //   fetchDataBasedOnSearch();
-  // }, []);
+  useEffect(() => {
+    getCocktails();
+  }, [drinks]);
+
   const getText = (text) => {
     setTextSearch(text);
     console.log(
@@ -39,28 +54,6 @@ const AppProvider = ({ children }) => {
       textSearch
     );
   };
-  // const searchTvShow = async () => {
-  //   if (!searchQuery || searchQuery.trim() === "") return;
-
-  //   setLoading(true);
-  //   setNoTvShows(false);
-
-  //   const URL = prepareSearchQuery(searchQuery);
-
-  //   const response = await axios.get(URL).catch((err) => {
-  //     console.log("Error: ", err);
-  //   });
-
-  //   if (response) {
-  //     console.log("Response: ", response.data);
-  //     if (response.data && response.data.length === 0) setNoTvShows(true);
-
-  //     setTvShows(response.data);
-  //   }
-
-  //   setLoading(false);
-  // };
-  // useDebounce(textSearch, 5000, getText);
   return (
     <AppContext.Provider
       value={{
@@ -69,6 +62,7 @@ const AppProvider = ({ children }) => {
         textSearch,
         getText,
         drinks,
+        cocktails,
       }}
     >
       {children}

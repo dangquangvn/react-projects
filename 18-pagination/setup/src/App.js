7d92dd250963baa8ec1919c6286213fb525a9a7f
quangTD <tsquang398@gmail.com>
2021-12-28 15:43:14 +0700

    :bug: [FIXME] too many re-renders

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -1,8 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const [person, setPerson] = useState();
+  const { loading, data: personData } = useFetch();
+  console.log("🚀TCL: ~ file: App.js ~ line 6 ~ App ~ person", personData);
+
+  // if (loading) {
+  const { avatar_url: image, id, login: name } = personData;
+  const newPerson = { image, id, name };
+  setPerson(newPerson);
+  // } else {
+  //   setPerson([]);
+  // }
+
+  // const { image, id, name } = person;
+  return (
+    <main>
+      <div className='section-title'>
+        <h2>pagination starter</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='list'>{/* <img src={image} alt={name} /> */}</div>
+    </main>
+  );
 }
 
-export default App
+export default App;

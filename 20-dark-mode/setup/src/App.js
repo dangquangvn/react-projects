3ae1cddf35e3423e8685109d4ffbe4897f2a7d9f
quangTD <tsquang398@gmail.com>
2022-01-04 22:07:24 +0700

    :🎉: darkmode setup

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const handleDarkMode = () => {
+    console.log("dark");
+  };
+  return (
+    <main className='dark-theme'>
+      <nav>
+        <div className='nav-center'>
+          <h1>Overreacted</h1>
+          <button className='btn' onClick={handleDarkMode}>
+            Toggle
+          </button>
+        </div>
+        <div className='articles'>
+          {data && data.map((item) => <Article key={item.id} {...item} />)}
+        </div>
+      </nav>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,7 +1,41 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from "react";
+import moment from "moment";
 
-export default Article
+const getOrdinalNum = function (d) {
+  if (d > 3 && d < 21) return "th";
+  switch (d % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+};
+
+const Article = ({ id, title, date, length, snippet }) => {
+  return (
+    <article className='post'>
+      <h2>{title}</h2>
+      <div className='post-info'>
+        <p>
+          {new Intl.DateTimeFormat("en-GB", {
+            weekday: "long",
+            day: "numeric",
+            year: "numeric",
+          }).format(date)}
+          {/* {Intl.DateTimeFormat("en-US", {
+            weekday: "long",
+            year: "numeric",
+          }).format(date)} */}
+          <span>{length} min read</span>
+        </p>
+        <p>{snippet}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Article;

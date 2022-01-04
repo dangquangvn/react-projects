0782537a541c8b1116e78ee31a4364607ffa9e67
quangTD <tsquang398@gmail.com>
2022-01-04 23:07:26 +0700

    :sparkles: [21 MOVIE DB] react router setup

--- 21-movie-db/setup/src/App.js
+++ 21-movie-db/setup/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Error from "./Error";
 
-import Home from './Home'
-import Movie from './SingleMovie'
+import Home from "./Home";
+import Movie from "./SingleMovie";
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/movies/:id' children={<Movie />} />
+      <Route exact path='*'>
+        <Error />
+      </Route>
+    </Switch>
+  );
 }
 
-export default App
+export default App;

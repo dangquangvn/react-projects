c08ee1385aa06078e505475d3455b05ad811dd29
quangTD <tsquang398@gmail.com>
2022-01-04 22:58:05 +0700

    :sparkles: [20 DARK MODE] 212. localStorage

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from "react";
 import data from "./data";
 import Article from "./Article";
 
+const getThemeStorage = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 function App() {
-  const [theme, setTheme] = useState("light-theme");
+  const [theme, setTheme] = useState(getThemeStorage());
   const toggleTheme = () => {
-    if (theme === "light-theme") {
+    const value = localStorage.getItem("theme");
+    // if (!value) return;
+    if (value === "light-theme") {
       console.log("dark");
       setTheme("dark-theme");
     } else {
@@ -17,6 +27,7 @@ function App() {
   useEffect(() => {
     console.log(document.documentElement.className);
     document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (

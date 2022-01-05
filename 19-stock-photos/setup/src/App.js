bc435b612539ae4c917ff282a86701e825169c2c
quangTD <tsquang398@gmail.com>
2022-01-05 11:46:06 +0700

    :sparkles: [19 STOCK PHOTOS V2] basic structure and fetch image

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [photos, setPhotos] = useState([]);
+  console.log("🚀TCL: ~ file: App.js ~ line 10 ~ App ~ photos", photos);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const fetchPhotos = async () => {
+    setLoading(true);
+    const pageUrl = `&page=${page}&per_page=9`;
+    let url = `${mainUrl}${clientID}${pageUrl}`;
+    try {
+      console.log(url);
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <main>
+      <section className='search'>
+        <form action='' className='search-form'>
+          <input type='text' className='form-input' placeholder='Search' />
+          <button type='submit' className='submit-btn'>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((photo) => (
+            <Photo key={photo.id} {...photo} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({
+  user: {
+    name,
+    profile_image: { small: avatar },
+  },
+  urls: { regular: image },
+  likes,
+}) => {
+  return (
+    <article className='photo'>
+      <img src={image} alt={name} />
+      <div className='photo-info'>
+        <div className=''>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <img src={avatar} alt={name} className='user-img' />
+      </div>
+    </article>
+  );
+};
 
-export default Photo
+export default Photo;

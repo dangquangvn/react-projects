48dccd5eb343cead9518500f4fb4424f006b735a
quangTD <tsquang398@gmail.com>
2022-01-05 11:11:26 +0700

    :🎉: [21 MOVIE DB] basic setup

--- 21-movie-db/setup/src/Home.js
+++ 21-movie-db/setup/src/Home.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import Form from './SearchForm'
-import Movies from './Movies'
+import React, { useState } from "react";
+import Form from "./SearchForm";
+import Movies from "./Movies";
 const Home = () => {
-  return <h2>movie component</h2>
-}
+  const [searchQuery, setSearchQuery] = useState("batman");
+  const [movies, setMovies] = useState([]);
+  return (
+    <main className='section'>
+      <form className='search-form'>
+        <h2>Search Movies</h2>
+        <input
+          type='text'
+          className='form-input'
+          placeholder='movie name...'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </form>
+      <section className='movies'></section>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
